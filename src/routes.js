a0d7d085d879137ea4e927450604ad453e621bab
kiranbabup <kiran12146@gmail.com>
2024-02-07 17:57:53 +0530

    backend static test3

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,15 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useParams } from "react-router-dom";
 import MenuPage from "./pages/MenuPage";
 
 const Router = () => {
+  const { id } = useParams();
+    const urlParams = new URLSearchParams(window.location.search);
+    const foodListParam = urlParams.get('foodList');
+    const foodList = JSON.parse(decodeURIComponent(foodListParam));
   return (
     <Routes>
         {/* <Route path="/" element={<MenuPage />} /> */}
-        <Route path="/customer-menu/:id" element={<MenuPage />} />
+        <Route path="/customer-menu/:id" element={<MenuPage foodList={foodList} id={id}/>} />
     </Routes>
   );
 };
