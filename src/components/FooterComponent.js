a4ddccdfed8be0397f350aab9a27073c5d735996
kiranbabup <kiran12146@gmail.com>
2024-02-08 17:01:16 +0530

    call and map added

--- src/components/FooterComponent.js
+++ src/components/FooterComponent.js
@@ -1,5 +1,7 @@
-import { Box, Button, styled } from "@mui/material";
+import { Box, Button, IconButton, Tooltip, styled } from "@mui/material";
 import CallIcon from '@mui/icons-material/Call';
+import RateReviewIcon from '@mui/icons-material/RateReview';
+import Zoom from '@mui/material/Zoom';
 
 const FooterComponent = () => {
     const FooterBox = styled(Box)({
@@ -11,13 +13,22 @@ const FooterComponent = () => {
         borderTop: "1px dashed gray"
     })
     const handleCallUsClick = () => {
-        // Replace the phone number with your desired number
-        const phoneNumber = "8374409756";
+        const phoneNumber = "7207486526";
         window.location.href = `tel:${phoneNumber}`;
     };
+    const handleRateUsClick=()=>{
+        const googleMapsUrl = "https://maps.app.goo.gl/ZuS5XfTX5DYakDpV6";
+        window.open(googleMapsUrl, '_blank');
+    }
+    
     return (
         <FooterBox>
-            <Button variant="outlined" startIcon={<CallIcon />} onClick={handleCallUsClick}>Call Us</Button>
+            <Tooltip title="Call Us" placement="top" arrow sx={{fontSize:"20px"}} TransitionComponent={Zoom}>
+                <IconButton sx={{ border: "2px solid",}} color="primary" onClick={()=>handleCallUsClick()} ><CallIcon /></IconButton>
+            </Tooltip>
+            <Tooltip title="Rate Us" placement="top" arrow sx={{fontSize:"20px"}} TransitionComponent={Zoom}>
+                <Button sx={{ border: "2px solid", }} variant="outlined" onClick={()=>handleRateUsClick()}><RateReviewIcon fontSize="large" /></Button>
+            </Tooltip>
         </FooterBox>
     )
 }
