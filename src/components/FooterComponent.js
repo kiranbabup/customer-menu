ffd0c9eeeae9849a33997bc2660a8b3a20dbcfce
kiranbabup <kiran12146@gmail.com>
2024-02-08 17:49:32 +0530

    added social media test 1

--- src/components/FooterComponent.js
+++ src/components/FooterComponent.js
@@ -2,6 +2,9 @@ import { Box, Button, IconButton, Tooltip, styled } from "@mui/material";
 import CallIcon from '@mui/icons-material/Call';
 import RateReviewIcon from '@mui/icons-material/RateReview';
 import Zoom from '@mui/material/Zoom';
+import FacebookRoundedIcon from '@mui/icons-material/FacebookRounded';
+import XIcon from '@mui/icons-material/X';
+import InstagramIcon from '@mui/icons-material/Instagram';
 
 const FooterComponent = () => {
     const FooterBox = styled(Box)({
@@ -20,14 +23,35 @@ const FooterComponent = () => {
         const googleMapsUrl = "https://maps.app.goo.gl/ZuS5XfTX5DYakDpV6";
         window.open(googleMapsUrl, '_blank');
     }
-    
+    const handleFBClick=()=>{
+        const fburl ="https://www.facebook.com/panigrahi.kiranbabu?mibextid=ZbWKwL";
+        window.open(fburl, '_blank');
+    }
+    const handleTXClick=()=>{
+        const txurl ="https://twitter.com/PANIGRAHIKIRAN3";
+        window.open(txurl, '_blank');
+    }
+    const handleIGClick=()=>{
+        const igurl ="https://www.instagram.com/explore/locations/161221201159335/rk-bar-and-restaurant/";
+        window.open(igurl, '_blank');
+    }
+
     return (
         <FooterBox>
-            <Tooltip title="Call Us" placement="top" arrow sx={{fontSize:"20px"}} TransitionComponent={Zoom}>
-                <IconButton sx={{ border: "2px solid",}} color="primary" onClick={()=>handleCallUsClick()} ><CallIcon /></IconButton>
+            <Tooltip title="Call Us" placement="top" arrow TransitionComponent={Zoom}>
+                <CallIcon fontSize="large"  color="primary" onClick={()=>handleCallUsClick()}/>
             </Tooltip>
-            <Tooltip title="Rate Us" placement="top" arrow sx={{fontSize:"20px"}} TransitionComponent={Zoom}>
-                <Button sx={{ border: "2px solid", }} variant="outlined" onClick={()=>handleRateUsClick()}><RateReviewIcon fontSize="large" /></Button>
+            <Tooltip title="Rate Us" placement="top" arrow TransitionComponent={Zoom}>
+                <RateReviewIcon fontSize="large" color="primary" onClick={()=>handleRateUsClick()}/>
+            </Tooltip>
+            <Tooltip title="Visit our Facebook Page" placement="top" arrow TransitionComponent={Zoom}>
+                <FacebookRoundedIcon fontSize="large" color="primary" onClick={()=>handleFBClick()}/>
+            </Tooltip>
+            <Tooltip title="Visit our Twitter Page" placement="top" arrow TransitionComponent={Zoom}>
+                <XIcon fontSize="large" color="primary" onClick={()=>handleTXClick()}/>
+            </Tooltip>
+            <Tooltip title="Visit our Insta Page" placement="top" arrow TransitionComponent={Zoom}>
+                <InstagramIcon fontSize="large" color="primary" onClick={()=>handleIGClick()}/>
             </Tooltip>
         </FooterBox>
     )
